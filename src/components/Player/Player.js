2c4decfee5ceb0d39541490d5a2b98853013bc44
ismail360deg <ismailbhuiyan2000@gmail.com>
2022-09-29 13:05:05 +0600

    break and exercise details done

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -36,6 +36,25 @@ const Player = () => {
                 <img src={logo1} alt="" />
                 <h3>Nazmul Hossen Papun</h3>
                 <p>President of Bangladesh Cricket Board</p>
+                <h2>Add A Break</h2>
+                <div className='Break-info'>
+                    <button>10h</button>
+                    <button>20h</button>
+                    <button>30h</button>
+                    <button>40h</button>
+                </div>
+
+                <h2>Exercise Details</h2>
+                <div className='exercisee-time'>
+                    <h3>Exercise Time</h3>
+                    <h3 className='time-seconds'>200 seconds</h3>
+                </div>
+                <div className='break-time'>
+                    <h3>Break Time</h3>
+                    <h3 className='time-seconds'>15 seconds</h3>
+                </div>
+
+
                 <h1>seleted item {cart.length}</h1>
             </div>
         </div>
